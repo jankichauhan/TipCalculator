125dc46af5d87a1eb8295ee991ec7dc55906eaee
Janki <janki.chauhan@gmail.com>
2015-03-27 13:39:08 -0700

    Initial Commit

---
+++ tipcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator
+//
+//  Created by Janki on 3/27/15.
+//  Copyright (c) 2015 Janki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
